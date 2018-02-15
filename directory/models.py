09cd204431c6e4c6010fdf72fba5b7e725f1fe96
Drew Atkinson <drewatkinson5@gmail.com>
2018-02-15 15:54:27 -0500

    Remove user model and add SocialField model

--- directory/models.py
+++ directory/models.py
@@ -1,28 +1,14 @@
-from django.contrib.postgres.fields import JSONField
+from django.conf import settings
 from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
 
-class User(models.Model):
-    username = models.CharField(max_length=64)
-    password = models.CharField(max_length=256) # TODO: Figure out how to make this work with Django's built in auth
-    name = models.CharField(max_length=256)
-    time_created = models.DateTimeField()
-    last_modified = models.DateTimeField()
-
-    ADMIN = 1
-    USER = 0
-    USER_TYPE_CHOICES = (
-        (ADMIN, 'Admin'),
-        (USER, 'User')
-    )
-    user_type = models.IntegerField(
-        choices=USER_TYPE_CHOICES,
-        default=USER
-    )
-
 class Profile(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     name = models.CharField(max_length=256)
     category = models.ManyToManyField(Category)
     offers_private_lessons = models.BooleanField(default=False)
@@ -31,8 +17,10 @@ class Profile(models.Model):
     website = models.URLField(max_length=256)
     phone = models.CharField(max_length=20)
     profile_img_url = models.URLField(max_length=256)
-    social = JSONField()
     time_created = models.DateTimeField()
     last_modified = models.DateTimeField()
 
-
+class SocialField(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    type = models.CharField(max_length=256)
+    url = models.URLField()
